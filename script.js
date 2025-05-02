8e74ee1dfffacfaf4a78c3de2d2485f1d4aa531d
Aviii7 <jacdaguro22@src.pshs.edu.ph>
2025-05-02 12:14:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+  const name = document.cookie.split('; ').find(row => row.startsWith('username='));
+  if (name) {
+    const value = name;
+    alert('Welcome back,' + value + '!' );
+  }
+}
+
+function storeUsername(e) {
+  const name = document.getElementById("username").value;
+
+ 
+  if (name.trim() === "" || !/^[a-zA-Z0-9]+$/.test(name)) {
+    alert("Please enter a valid username (letters and numbers only).");
+    return;
+  }
+
+  document.cookie = 'username=' + name;
+  alert('Welcome, ' + name + '! Get ready to jump!');
+}
